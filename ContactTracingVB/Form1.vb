7da5aa74df2e8f834d552518bb1f8e9f9b656b6d
jacobasuncion <asuncion1173@gmail.com>
2022-06-11 03:20:12 +0800

    Coded file writer

--- ContactTracingVB/Form1.vb
+++ ContactTracingVB/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
+    Private Sub submitBtn_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
+        Dim visitorFile As System.IO.StreamWriter
 
+        visitorFile = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\ASUNCION\Desktop\Visitor's Files\Visitor.txt", True)
+        visitorFile.WriteLine(firstTxtBox.Text)
+    End Sub
 End Class
