4a343c5a7bc0db6e0d8af7d3075859d66ef0d544
jacobasuncion <asuncion1173@gmail.com>
2022-06-11 04:23:44 +0800

    Updated Design

--- ContactTracingVB/Form1.Designer.vb
+++ ContactTracingVB/Form1.Designer.vb
@@ -241,6 +241,7 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Stay Safe"
         Me.ResumeLayout(False)
         Me.PerformLayout()
