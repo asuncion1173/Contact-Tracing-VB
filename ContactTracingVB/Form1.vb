16437b3f3f0cdffc6c2d903cbc50e1f4a54d1fd1
jacobasuncion <asuncion1173@gmail.com>
2022-06-11 03:28:33 +0800

    Concatenated message on each fill up line

--- ContactTracingVB/Form1.vb
+++ ContactTracingVB/Form1.vb
@@ -3,14 +3,14 @@
         Dim visitorFile As System.IO.StreamWriter
 
         visitorFile = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\ASUNCION\Desktop\Visitor's Files\Visitor.txt", True)
-        visitorFile.WriteLine(firstTxtBox.Text)
-        visitorFile.WriteLine(midTxtBox.Text)
-        visitorFile.WriteLine(lastTxtBox.Text)
-        visitorFile.WriteLine(residenceTxtBox.Text)
-        visitorFile.WriteLine(contactTxtBox.Text)
-        visitorFile.WriteLine(genderTxtBox.Text)
-        visitorFile.WriteLine(emailTxtBox.Text)
-        visitorFile.WriteLine(dateTxtBox.Text)
+        visitorFile.WriteLine("First Name: " + firstTxtBox.Text)
+        visitorFile.WriteLine("Middle Initial: " + midTxtBox.Text)
+        visitorFile.WriteLine("Last Name: " + lastTxtBox.Text)
+        visitorFile.WriteLine("Residence: " + residenceTxtBox.Text)
+        visitorFile.WriteLine("Contact Number: " + contactTxtBox.Text)
+        visitorFile.WriteLine("Gender: " + genderTxtBox.Text)
+        visitorFile.WriteLine("Email: " + emailTxtBox.Text)
+        visitorFile.WriteLine("Date of Visit: " + dateTxtBox.Text)
         visitorFile.Close()
         MessageBox.Show("Thank you for submitting! Stay safe and always wear your mask!")
     End Sub
